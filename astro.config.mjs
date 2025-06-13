eb3cab283b6036bc5181bbe8820460d64ec85fa3
NhanPham03 <ph.nhan03@gmail.com>
2025-06-13 20:30:37 +0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import mdx from '@astrojs/mdx';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    devImageService: 'sharp',
+  })
+});
