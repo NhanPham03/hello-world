b83e6eed5cad3380049d84fa56a156b173c432dc
NhanPham03 <ph.nhan03@gmail.com>
2025-07-02 16:07:23 +0700

    feat(api): add endpoint to retrieve posts

---
+++ src/pages/api/posts.json.ts
@@ -0,0 +1,14 @@
+import { getCollection } from "astro:content";
+
+export async function GET() {
+  const posts = await getCollection("post");
+  // @ts-expect-error
+  posts.map(post => post.data.pubDate = post.data.pubDate.toLocaleDateString());
+
+  return new Response(JSON.stringify(posts), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
